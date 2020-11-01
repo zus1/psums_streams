a738468161d18438660ea4662ab4426832e96763
Mario O <zus.ozus@gmail.com>
2020-11-01 11:13:19 +0100

    some cleanup

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
 <?php
 include_once("include.php");
-/*$api = Factory::getApi(Factory::API_AGGREGATOR);
-$params = array("id" => "1a2b3c4d", 'sign' => "14rg7-7un89-da234-ng5p", "stream" => "lorem ipsum dipsum, tipsum..");
-$res = $api->sendToAggregator($params);
-var_dump($res);
-die();*/
-$cnt = Factory::getObject(Factory::TYPE_STREAM_CONTROLLER);
-$res = $cnt->cycleStreams();
-var_dump($res);
-die();
 echo "Nothing here";
